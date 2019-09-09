b3de7dce664e8bd2a7b6809ce2b749d2da598a51
Muhammed Baldeh <alagiesellu@gmail.com>
2019-09-09 12:16:05 +0000

    done asset creation. now going to find by filed value.

--- src/main/java/data/repository/BigchainRepository.java
+++ src/main/java/data/repository/BigchainRepository.java
@@ -2,45 +2,64 @@ package data.repository;
 
 import data.annotations.Entity;
 import java.lang.reflect.Field;
+import java.util.ArrayList;
+
 import org.json.JSONObject;
 
 public class BigchainRepository<E> {
     
-    private E type;
-    private Field[] fields;
+    private E type; // Object to be mapped
+    private Field[] fields; // all variables of the object
     
+    // process Object to be mapped
     public BigchainRepository(Class type) {
         
         this.type = (E) type;
-        boolean exit = false;
         
-        if (type.getAnnotations().length != 1 && !type.isAnnotationPresent(Entity.class)) {
-            System.err.println(getTableName() + " can only have @Entity annotation.");
-            exit = true; // error require exit
-        }
-    
-        // load all fields found in class type E
+        // keep track errors
+        ArrayList<String> errors = new ArrayList<String>();
+        
+        if (
+                type.getAnnotations().length != 1 // object can only have one annotation
+                        && // and
+                        !type.isAnnotationPresent(Entity.class) // annotation must be @Entity
+        )
+            errors.add(getTableName() + " can only have @Entity annotation.");
+        
+        // load all fields found in object to be mapped
         this.fields = type.getDeclaredFields();
     
-        for (Field field : this.fields) {
-            if ((field.getModifiers() & 1) == 0) {
-                System.err.println("Filed " + field.getName() + " must be public to be accessible.");
-                exit = true; // error require exit
-            }
-        }
+        // go though all fields
+        for (Field field : this.fields)
+            if ((field.getModifiers() & 1) == 0) // field must be public
+                errors.add("Filed " + field.getName() + " must be public to be accessible.");
         
-        // exit if error that required exit found
-        if (exit) System.exit(0);
+        // if there are errors
+        if (errors.size() > 0) {
+            for (String error : errors) // print all errors
+                System.err.println("- " + error);
+            System.exit(0); // then exit
+        }
     }
     
+    // pass an object then generate asset to be stored in db
     public void create(Object record)
     {
         JSONObject jsonObject = new JSONObject();
+        
         jsonObject.put("asset", this.getAsset(record));
         
-        System.out.println(jsonObject.toString());
+        System.out.println(jsonObject);
     }
     
+    public E find(String filed, Object value)
+    {
+        return null;
+    }
+    
+    /**
+     * Load object into JSON object.
+     * */
     private String getAsset(Object record)
     {
         JSONObject jsonObject = new JSONObject();
